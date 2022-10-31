ddace22906576363f8a872509808df3409790051
khengyun <khgammingcraft@gmail.com>
2022-10-31 20:27:57 +0700

    update @v2.0.0

--- dev.js
+++
@@ -1,9 +0,0 @@
-document.dispatchEvent(
-  new CustomEvent("RW759_connectExtension", {
-    detail: {AccessToken: AccessToken,
-             UserName: UserName,
-             UserID:UserID,
-              }
-  })
-);
-
